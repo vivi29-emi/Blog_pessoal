e4ae4c4e283188084e09fe0e9a80ad1acdfbeacd
Viviane <vivi29emi@gmail.com>
2022-05-16 20:55:27 -0300

    Adicionando configurações para o deploy do blog

--- src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
@@ -59,7 +59,7 @@ public class UsuarioController {
         return usuarioService.cadastrarUsuario(usuario)
             .map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
             .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
-		
+		                                      
 
 	}
 	@PutMapping("/atualizar")
--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
@@ -27,7 +29,8 @@ public class Usuario {
 	@NotNull(message = "O atributo Nome é Obrigatório!")
 	private String nome;
 	
-	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@Schema(example = "email@email.com.br")
+    @NotNull(message = "O atributo Usuário é Obrigatório!")
 	@Email(message = "O atributo Usuário deve ser um email válido!")
 	private String usuario;
 	
